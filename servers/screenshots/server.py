364a8726b83f9679c2ef386e98e9b46e29bb2915
Mislav <mislav@pop-os.localdomain>
2021-05-30 20:08:56 +0200

    Changed file upload

--- servers/screenshots/server.py
+++ servers/screenshots/server.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask.globals import request
-from flask_restful import Resource, Api, reqparse   
+from flask_restful import Resource, Api, reqparse
+import base64
 
 app = Flask(__name__)
 api = Api(app)
 
 screen_args = reqparse.RequestParser()
-screen_args.add_argument("image", type=str, required=True)
+screen_args.add_argument("image")
 
 class FetchScreenshot(Resource):
-    def post(self):
-        args = screen_args.parse_args()
-        print(args)
+    def post(self, request):
+        file = request.FILE
+        print(file)
         return "received"
 
 api.add_resource(FetchScreenshot, '/')
